28643dba8fe92d383e2a5fcfe93be3d0cdb7e438
Diogo Soares <diogo.soares@gnosis.pm>
2021-10-05 18:38:05 +0200

    test line 45

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components/macro";
+import TextFieldMui, { TextFieldProps } from "@mui/material/TextField";
+import "./App.css";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  align-items: center;
+`;
+
+type BasicTextFieldProps = {
+  basic: string;
+};
+
+type ExtendedTextFieldProps = TextFieldProps & ConditionalProps;
+
+type ConditionalProps =
+  | {
+      dummy: number;
+      foo?: never;
+    }
+  | {
+      foo?: string;
+    };
+
+const StyledTextField = styled(TextFieldMui)<TextFieldProps & ConditionalProps>`
+  & .MuiInputLabel-root {
+    color: #bbac57;
+    text-decoration: underline;
+  }
+`;
+
+const CustomTextField = (
+  props: TextFieldProps & BasicTextFieldProps & ConditionalProps
+) => {
+  console.log({ ...props });
+  return <StyledTextField {...props} />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>CustomTextField</Title>
+      <CustomTextField basic="basic" />
+      <CustomTextField placeholder="hold in there" basic="ola" />
+      <CustomTextField basic="ola" dummy={9} foo="yeha" />
+    </Wrapper>
   );
 }
 
